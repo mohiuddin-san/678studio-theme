9be850e056712c6a2d4a689010efdf97d0c8b070
mohiuddin-san <mohi@san-developer.com>
2025-07-25 18:03:59 +0900

    navigation update

--- template-parts/header/navigation.php
+++ template-parts/header/navigation.php
@@ -3,7 +3,7 @@
     <li class="navigation__item"><a href="<?php echo esc_url(home_url('/')); ?>" class="navigation__link">トップ</a></li>
     <li class="navigation__item"><a href="<?php echo esc_url(home_url('/about')); ?>"
         class="navigation__link">写真館を探す</a></li>
-    <li class="navigation__item"><a href="<?php echo esc_url(home_url('/portfolio')); ?>"
+    <li class="navigation__item"><a href="<?php echo esc_url(home_url('/studio-reservation')); ?>"
         class="navigation__link">ご予約</a></li>
     <li class="navigation__item"><a href="<?php echo esc_url(home_url('/contact')); ?>"
         class="navigation__link">お問い合わせ</a></li>
