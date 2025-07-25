671afdd9fb8482c79a0eb3c17a8797c6f7a4908b
Junichi <yoshihara@san-creation.com>
2025-07-26 08:07:55 +0900

    mohi san's code merge to change page title

--- html/wp-content/themes/678studio/page-studio-reservation.php
+++ html/wp-content/themes/678studio/page-studio-reservation.php
@@ -1,3 +1,9 @@
+<?php
+/*
+Template Name: スタジオ予約
+*/
+?>
+
 <?php get_header(); ?>
 <main class="main-content" style="background-color: #E9EDF0;">
   <?php get_template_part('template-parts/sections/reservation'); ?>
